e5f4bbf07834de04f02edb155808636efe10a703
will-shaw <will_shaw@live.com>
2017-04-03 11:29:19 +1200

    Updated adjacent calculation. Possibly fixed confusion after finding black immediately after rotating

--- assignment.c
+++ assignment.c
@@ -16,14 +16,13 @@ const int wheelCircum = 134;
 const int speed = 50;
 const int turnSpeed = 30;
 const int tileWidth = 95;
-const int laserOffset = 20;
+const int laserOffset = 30;
 
+const int towerDistance = 1700;
 const int reflectedBlack = 20;
-const int correctionRadius = 150;
-const int correctionDistance = 80;
+const int correctionRadius = 130;
+const int correctionDistance = 360; //in encoder counts
 const int correctionIncrement = 20;
-
-string numbers[] = {"One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten"};
 
 bool missionComplete = false;
 bool isBlack = false;
@@ -32,18 +31,17 @@ bool bumped = false;
 
 int tileCount = 0;
 
+/* Calculates encoder counts required to travel the specified distance. */
 float lengthToDegrees(float value) {
 	return ((value / wheelCircum) * 360);
 }
 
-//float degreesToLength(float value) {
-//	return (360 / (value * wheelCircum));
-//}
-
+/* Calculates angle from adjacent and opposite. */
 float getTurnAngle(float adj) {
-	return atan(correctionRadius/adj);
+	return radiansToDegrees(atan(correctionRadius/adj));
 }
 
+/* Plays lost tone sequence. */
 void lost() {
 	playTone(780, 50);
 	sleep(200);
@@ -52,24 +50,29 @@ void lost() {
 	playTone(300, 100);
 }
 
+/* Function to perform a 90 degree pivot. Takes a direction to turn. */
 void pivot(int direction) {
 	setMotorSyncEncoder(leftMotor, rightMotor, direction * 100, 180, turnSpeed);
 	waitUntilMotorStop(leftMotor);
 	waitUntilMotorStop(rightMotor);
 }
 
+/* Performs a pivot of a specified angle in a specified direction. */
 void pivot(int direction, int angle) {
 	setMotorSyncEncoder(leftMotor, rightMotor, direction * 100, angle, turnSpeed);
 	waitUntilMotorStop(leftMotor);
 	waitUntilMotorStop(rightMotor);
 }
 
+/* Drives straight until not detecting black. */
 void moveToTileEdge() {
 	while(isBlack) {
 		setMotorSync(leftMotor, rightMotor, 0, speed);
+		displayCenteredBigTextLine(4, "Go To Edge");
 	}
 }
 
+/* Uses sonar to pivot and scan for an object. */
 void scanForObject() {
 	pivot(-1);
 	int totalPivot = 0;
@@ -77,51 +80,92 @@ void scanForObject() {
 		pivot(1, 30);
 		totalPivot += 30;
 	}
+	pivot(1, 50);
 }
 
+/* Begins corrective movement left or right. Returns distance it drove in encoder counts. */
 int correctiveMove() {
+	playTone(500, 20);
+	sleep(20);
+	playTone(1000, 20);
 	int adjacent = 0;
+
 	while(!isBlack && adjacent < correctionDistance) {
 		setMotorSyncEncoder(leftMotor, rightMotor, 0, correctionIncrement, speed);
+		displayCenteredBigTextLine(4, "Driven: %d", adjacent);
 		adjacent += correctionIncrement;
 		waitUntilMotorStop(leftMotor);
+		waitUntilMotorStop(rightMotor);
+	}
+
+	if (isBlack) {
+		setMotorSyncEncoder(leftMotor, rightMotor, 0, 100, speed);
+		waitUntilMotorStop(leftMotor);
+		if (isBlack) {
+			return adjacent;
+		}
 	}
 	return adjacent;
 }
 
-bool correctiveRealign(int direction, float angle) {
+/* Begins corrective realign, takes an angle to pivot after moving to center of tile. */
+void correctiveRealign(int direction, float angle) {
+	playTone(1000, 20);
+	sleep(20);
+	playTone(500, 20);
 	if (isBlack) {
-		setMotorSyncEncoder(leftMotor, rightMotor, 0, lengthToDegrees((tileWidth/2) + laserOffset), speed);
+		setMotorSyncEncoder(leftMotor, rightMotor, 0, lengthToDegrees(tileWidth/3), speed);
 		waitUntilMotorStop(leftMotor);
-		pivot(direction, angle);
-		return true;
+		displayCenteredBigTextLine(4, "Aligning");
+		pivot(direction, angle * 2);
 	}
-	return false;
 }
 
-/* If we expect a tile but don't find one; calculate correction */
+/* If we expect a tile but don't find one; take corrective action. */
 void correction() {
 
 	pivot(-1);
+
+	//setMotorSyncEncoder(leftMotor, rightMotor, 0, lengthToDegrees(laserOffset / 2), -1 * speed);
+	//waitUntilMotorStop(leftMotor);
 	int adjacent = correctiveMove();
-	if (correctiveRealign(1, getTurnAngle(adjacent))) {
+	if (adjacent < correctionDistance) {
+		if (adjacent == 0) {
+		 	correctiveRealign(1, 90);
+		} else {
+			correctiveRealign(1, getTurnAngle(adjacent));
+		}
+		displayCenteredBigTextLine(4, "Adj: %d", adjacent);
 		return;
 	}
 
 	pivot(1, 360);
-	setMotorSyncEncoder(leftMotor, rightMotor, 0, correctionDistance, speed);
+	setMotorSyncEncoder(leftMotor, rightMotor, 0, (correctionDistance + 100 + lengthToDegrees(laserOffset / 2)) / 2, speed);
 	waitUntilMotorStop(leftMotor);
 
 	adjacent = correctiveMove();
-	if (correctiveRealign(-1, getTurnAngle(adjacent))) {
+	if (adjacent < correctionDistance) {
+		if (adjacent == 0) {
+			correctiveRealign(-1, 90);
+		} else {
+			correctiveRealign(-1, getTurnAngle(adjacent));
+		}
+		displayCenteredBigTextLine(4, "Adj: %d", adjacent);
 		return;
 	}
 
 	lost();
 }
 
+/* Performs a push to push the obstable off a tile. */
 void push() {
-	setMotorSyncEncoder(leftMotor,rightMotor, 0, 300, 100);
+	int powerLevel = 50;
+	while (powerLevel < 100) {
+		setMotorSyncEncoder(leftMotor,rightMotor, 0, lengthToDegrees(100), powerLevel);
+		sleep(10);
+		powerLevel ++;
+	}
+	setMotorSyncEncoder(leftMotor,rightMotor, 0, lengthToDegrees(300), powerLevel);
 	waitUntilMotorStop(leftMotor);
 }
 
@@ -133,8 +177,12 @@ task bumpCheck() {
 
 task sonarScan() {
 	while (!missionComplete) {
-		objectDetected = SensorValue[sonarSensor] < 600;
-		displayCenteredBigTextLine(4, "Detected");
+		objectDetected = SensorValue[sonarSensor] < 1000;
+		if (objectDetected) {
+			displayCenteredBigTextLine(4, "Detected");
+		} else {
+			displayCenteredBigTextLine(4, "");
+		}
 	}
 }
 
@@ -146,7 +194,7 @@ task updateColour() {
 
 /* Drive forward for a set number of wheel rotations. Then look for tower*/
 task stageThree() {
-	setMotorSyncEncoder(leftMotor, rightMotor, 0, lengthToDegrees(1400), speed);
+	setMotorSyncEncoder(leftMotor, rightMotor, 0, lengthToDegrees(towerDistance), speed);
 	waitUntilMotorStop(leftMotor);
 
 	startTask(bumpCheck, 255);
@@ -181,6 +229,7 @@ task stageTwo() {
 			moveToTileEdge();
 		}
 
+		displayCenteredBigTextLine(4, "Driving to Radius");
 		setMotorSyncEncoder(leftMotor, rightMotor, 0, lengthToDegrees(correctionRadius), speed);
 		waitUntilMotorStop(leftMotor);
 
@@ -207,9 +256,11 @@ task stageOne() {
 		setMotorSync(leftMotor, rightMotor, 0, speed);
 	}
 
-	setMotorSyncEncoder(leftMotor, rightMotor, 0, lengthToDegrees(tileWidth + laserOffset), speed);
+	setMotorSyncEncoder(leftMotor, rightMotor, 0, lengthToDegrees((tileWidth/2) + laserOffset), speed);
 	waitUntilMotorStop(leftMotor);
 	pivot(1);
+	playTone(800, 30);
+	tileCount++;
 	startTask(stageTwo, 255);
 }
 
